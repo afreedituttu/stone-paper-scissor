3674a5e7712ca7b6233dbadc347cea0a5f0e929d
afreedi <afreedisulfiker@gmai.com>
2022-11-18 22:06:59 +0530

    starting stage

---
+++ script.js
@@ -0,0 +1,10 @@
+const form = document.getElementById("user-form");
+const userDisplay = document.getElementById("user-selection");
+const computerDisplay = document.getElementById("computer-selection");
+const resultDisplay = document.getElementById("result-box");
+const userPoints = document.getElementById("points-computer");
+const computerPoints = document.getElementById("points-computer");
+
+form.addEventListener('click',()=>{
+    form.defa
+})
